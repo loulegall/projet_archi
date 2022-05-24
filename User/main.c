cd18a3dbdc391a5b3b775857297116eb81cb119a
loulegall <90677417+loulegall@users.noreply.github.com>
2022-05-24 13:09:29 +0200

    Update main.c

--- User/main.c
+++ User/main.c
@@ -1,9 +1,9 @@
 //===========================================================//
 // Projet Micro - INFO1 - ENSSAT - S2 2022							 //
 //===========================================================//
-// File                : Programme de départ
+// File                : Programme de dÃ©part
 // Hardware Environment: Open1768	
-// Build Environment   : Keil µVision
+// Build Environment   : Keil ÂµVision
 //===========================================================//
 
 #include "lpc17xx_gpio.h"
@@ -19,7 +19,7 @@
 #include "memoire_i2c.h"
 #include "lpc17xx_i2c.h"
 
-#include "globaldec.h" // fichier contenant toutes les déclarations de variables globales
+#include "globaldec.h" // fichier contenant toutes les dÃ©clarations de variables globales
 #include <stdio.h>
 
 //===========================================================//
@@ -27,18 +27,18 @@
 //===========================================================//
 int main(void)
 {
-	  int cptTouch = 0; // 
-		int n = 1; // variable qui permet de ne pas exécuter tous les 10ms dans le while
-		//uint8_t mesdatas[5] ={0,8,9,12,4}; // tableau de données
-	  uint8_t mesdatas[5] ;
-		uint8_t mesdataslu[5]; // tableau vide de données
+	  	int cptTouch = 0; // compteur 
+	  	int n = 1; // variable qui permet de ne pas exÃ©cuter tous les 10ms dans le while
+	  	//uint8_t mesdatas[5] ={0,8,9,12,4}; // tableau de donnÃ©es
+	  	uint8_t mesdatas[5] ;
+	  	uint8_t mesdataslu[5]; // tableau vide de donnÃ©es
 					
-		// Initialisation PINSEL pour accerder a la mémoire I2C
+		// Initialisation PINSEL pour accerder a la mÃ©moire I2C
 		PINSEL_CFG_Type funcSDA;
 		PINSEL_CFG_Type funcSCL;
 	
 		// initialisation pin SDA
-		funcSDA.Portnum = PINSEL_PORT_0; // numéro du port 0.
+		funcSDA.Portnum = PINSEL_PORT_0; // numÃ©ro du port 0.
 		funcSDA.Pinnum = PINSEL_PIN_27; // pin 0.27
 		funcSDA.Funcnum = PINSEL_FUNC_1 ; // Car function 01 => 1
 		funcSDA.Pinmode = PINSEL_PINMODE_PULLUP; //
@@ -46,7 +46,7 @@ int main(void)
 	
 	
 		// initialisation pin SCD
-		funcSCL.Portnum = PINSEL_PORT_0; // numéro du port 0.
+		funcSCL.Portnum = PINSEL_PORT_0; // numÃ©ro du port 0.
 		funcSCL.Pinnum = PINSEL_PIN_28; // pin 0.28
 		funcSCL.Funcnum = PINSEL_FUNC_1 ; // Car function 01 => 1
 		funcSCL.Pinmode = PINSEL_PINMODE_PULLUP;
@@ -59,8 +59,8 @@ int main(void)
 		lcd_Initializtion(); // init pinsel ecran et init LCD
 		touch_init(); //initialisation du tactile
 
-	  // direction du GPIO 0.1 en sortie 
-	  GPIO_SetDir(0, 0xF, 1);
+	 	 // direction du GPIO 0.1 en sortie 
+	  	GPIO_SetDir(0, 0xF, 1);
 	
 		initTimer(); // appelle de la fonction qui appelle la structure du timer
 		TIM_Cmd(LPC_TIM0, ENABLE); //lancement du timer
@@ -68,7 +68,7 @@ int main(void)
 
 		
 		//I2C
-	  init_i2c_eeprom();
+	  	init_i2c_eeprom();
 		
 		mesdatas[0] = cptTouch; // initialisation de la case d'indice 0
 		i2c_eeprom_write_read(30, &mesdatas[0], &mesdataslu[0], 5);
@@ -83,11 +83,12 @@ int main(void)
 				}
 			}
 			else {
+				// boucle qui permet d'afficher le nombre de tapes qu'on fait sur l'Ã©cran
 				if ( n == 0){	
 					
 						if(!(cptTouch%10)) // on enregistre tous les 10 fois la variable cpTouch
 						{
-							mesdatas[0] = cptTouch; // ajout variable qui compte le nrb de tape dans tableau de données
+							mesdatas[0] = cptTouch; // ajout variable qui compte le nrb de tape dans tableau de donnÃ©es
 							i2c_eeprom_write_read(30, &mesdatas[0], &mesdataslu[0], 5);
 							sprintf(chaine, "Stocke : %d",mesdataslu[0]);
 							LCD_write_english_string(10,40, chaine, White, 0);
